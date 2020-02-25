2d0cab27ea1c9b22bf033a193a7094d6a6c097ca
Ciolou <57500839+Ciolou1@users.noreply.github.com>
2020-02-25 16:13:35 -0600

    Loading Screen Work

--- fud-Alpha/www/js/index.js
+++ fud-Alpha/www/js/index.js
@@ -1,5 +1,21 @@
 /*
     Author: Fu-D Team
-    Last modified: 2/22/20
+    Last modified: 2/25/20
     Last modified by: Sam Lohbeck 
  */
+function loadingScreen () {
+    var preload = document.getElementById("load-wrapper");
+    var loading = 0;                                           
+    var id = setInterval(frame, 10);
+    
+    function frame() {
+        if (loading == 100) {
+            clearInterval(id);
+            window.open("../home.html","_self");
+        } else {
+            loading = loading + 1;
+            if(loading = 90) {
+                preload.style.animation = "fadeout 1s ease";
+            }
+        }
+    }
