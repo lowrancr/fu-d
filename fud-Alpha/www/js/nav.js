748f830b896253f6c1dcd6e292bd9c18285019b6
Ross Lowrance <55846368+lowrancr@users.noreply.github.com>
2020-03-03 14:43:16 -0600

    Create nav.js

---
+++ fud-Alpha/www/js/nav.js
@@ -0,0 +1,15 @@
+var navbar = '<ul>\
+              <li>\
+                <a href="#">Home</a>\
+              </li>\
+              <li>\
+                <a href="#">About Me</a>\
+              </li>\
+              <li>\
+                <a href="#">Contact</a>\
+              </li>\
+            </ul>';
+
+function loadNav() {
+    document.getElementById('nav').innerHTML = navbar;
+}
