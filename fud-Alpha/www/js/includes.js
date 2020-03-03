bd1aef6c0e6a6c08613f3a392b0ed49b7d663214
Ross Lowrance <55846368+lowrancr@users.noreply.github.com>
2020-03-03 14:56:46 -0600

    Includes Test
    -RL

--- fud-Alpha/www/js/includes.js
+++ fud-Alpha/www/js/includes.js
@@ -3,3 +3,10 @@
     Last modified: 2/27/20
     Last modified by: Sam Lohbeck
 */
+
+
+var footer = '../includes/Footer/footer.html';
+
+function includes() {
+    document.getElementById('footer').innerHTML = footer;
+}
