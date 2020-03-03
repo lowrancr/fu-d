b3035476f0c7bdd7461f6bfacf47c2aa310022ca
Ciolou <57500839+Ciolou1@users.noreply.github.com>
2020-03-03 15:13:56 -0600

    Includes Update

--- fud-Alpha/www/js/includes.js
+++ fud-Alpha/www/js/includes.js
@@ -4,13 +4,10 @@
     Last modified by: Sam Lohbeck
 */
 
-<<<<<<< HEAD
 // Footer Include
-=======
 
-var footer = '../includes/Footer/footer.html';
+var footer = '';
 
 function includes() {
-    document.getElementById('footer').innerHTML = footer;
+    document.getElementsByTagName('footer').innerHTML = footer;
 }
->>>>>>> bd1aef6c0e6a6c08613f3a392b0ed49b7d663214
