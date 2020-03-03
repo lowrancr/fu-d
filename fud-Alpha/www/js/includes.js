85ba2a03f20faf8fc5982c0ade376e2edb70265f
Ross Lowrance <55846368+lowrancr@users.noreply.github.com>
2020-03-03 15:08:51 -0600

    Update includes.js

--- fud-Alpha/www/js/includes.js
+++ fud-Alpha/www/js/includes.js
@@ -5,7 +5,9 @@
 */
 
 
-var footer = '../includes/Footer/footer.html';
+var footer = '<div class="col_1"><img src="theicon.png" /></div>\
+    <div class="col_2"><p>Copyright</p><p>Fu-D 2020</p></div>\
+    <div class="col_3"><p>Privacy Policy</p><p>User Agreement</p></div>';
 
 function includes() {
     document.getElementById('footer').innerHTML = footer;
